4125778ff3463d405668220d306a2cdfa0efd288
Sia <siakaramalegos@gmail.com>
2015-09-01 11:49:05 -0500

    Added cities list to location model and validated against that list

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,4 +5,10 @@ class Location < ActiveRecord::Base
   def my_location
     "#{address}, #{city}, GA"
   end
+
+  CITY = ["Atlanta", "Alpharetta", "Avondale Estates", "Chamblee", "Clarkston",
+    "College Park", "Decatur", "Doraville", "Dunwoody", "East Point",
+    "Fairburn", "Forest Park", "Hapeville", "Lithonia", "Morrow", "Palmetto",
+    "Riverdale", "Roswell"," Sandy Springs", "Stone Mountain", "Union City"]
+  validates :city, inclusion: CITY
 end
