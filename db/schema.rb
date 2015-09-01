623d5112de85d045dd896aec42fd5742f23f5b35
Sia <siakaramalegos@gmail.com>
2015-09-01 14:14:17 -0500

    Added distance to location table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901135209) do
+ActiveRecord::Schema.define(version: 20150901191243) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "address"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150901135209) do
     t.float    "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal  "distance"
   end
 
 end
